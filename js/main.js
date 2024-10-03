63a422d1ccd51502fa76780ba1b744bc8edc53ab
Zoya <byzyka1986@mail.ru>
2024-10-03 18:01:42 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+   
+      $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow:  '<button type="button" class="slick-arrow slick-next"><img src="images/next-top-slider.svg" alt="next"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev-top-slider.svg" alt="prev"></button>',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+             
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+                arrows: false,
+            }
+          },
+                    
+        ]
+      });
+
+      $('.news__items').masonry({
+      // options
+      itemSelector: '.news__item',
+      // columnWidth: 270,
+      gutter: 30,
+      fitWidth: true,
+      horizontalOrder: true
+    });
+
+    $('.advice-sliders').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      arrows: false,
+    });
+})
